9eebc0a3bacd0c09ea6d4dc974d7720fa88a1473
darkhunterbg <kaloyan.r.nachev@gmail.com>
2017-11-14 10:20:53 +0200

    Foxed missing project

---
+++ XenoCore.Desktop.Test/Game1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace XenoCore.Desktop.Test
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : XenoCore.Engine.XenoCoreGame
+    {
+        protected override void Initialize()
+        {
+            GraphicsDeviceManager.PreferredBackBufferWidth = 1280;
+            GraphicsDeviceManager.PreferredBackBufferHeight = 720;
+            GraphicsDeviceManager.ApplyChanges();
+
+            base.Initialize();
+        }
+    }
+}
